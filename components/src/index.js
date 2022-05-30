29504d2e876806076b5f9ce1d6fb6f7a08f83711
GustavoBauny <gustavounipam1504@gmail.com>
2022-05-30 16:35:17 -0300

    Correção

--- components/src/index.js
+++ components/src/index.js
@@ -13,10 +13,10 @@ function App() {
   return (
     <div className="App">
       Hello World
-      <Button onClick={() => soma(10, 20)} name="Bruno Carneiro" />
+      <Button onClick={() => soma(10, 20)} name="Botão 1" />
       <ComponentA>
         <ComponentB>
-          <Button onClick={() => soma(40, 20)} name="Aline Carneiro" />
+          <Button onClick={() => soma(40, 20)} name="Botão 2" />
         </ComponentB>
       </ComponentA>
     </div>
