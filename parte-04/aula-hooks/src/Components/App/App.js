1eaaad74de84000b121258eeb94225f850326091
GustavoBauny <gustavounipam1504@gmail.com>
2022-05-31 17:28:57 -0300

    Adicionando aula hooks

---
+++ parte-04/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Counter from '../Counter/Counter';
+import SmartCounter from '../Smartcounter.js/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+
+function App() {
+  return (
+    <div>
+      <h1>IFood</h1> 
+      <Ifoodcounter/>
+    </div>
+  )  
+}
+export default App;
+
